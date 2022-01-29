ff8022dd5457952d5fd96dccef928278d2b7e761
leonardo david moreno bedoya <leonardo.moreno@transmilenio.gov.co>
2022-01-29 11:44:51 -0600

    prueba

--- 1d-burgers/burgersutil.py
+++ 1d-burgers/burgersutil.py
@@ -41,14 +41,14 @@ def prep_data(path, N_u=None, N_f=None, N_n=None, q=None, ub=None, lb=None, nois
 
     if N_n != None and q != None and ub != None and lb != None and idx_t_0 != None and idx_t_1 != None:
       dt = t[idx_t_1] - t[idx_t_0]
-      idx_x = np.random.choice(Exact_u.shape[1], N_n, replace=False) 
+      idx_x = np.random.choice(Exact_u.shape[1], N_n, replace=False)
       x_0 = x[idx_x,:]
       u_0 = Exact_u[idx_t_0:idx_t_0+1,idx_x].T
       u_0 = u_0 + noise*np.std(u_0)*np.random.randn(u_0.shape[0], u_0.shape[1])
-        
+
       # Boudanry data
       x_1 = np.vstack((lb, ub))
-      
+
       # Test data
       x_star = x
       u_star = Exact_u[idx_t_1,:]
@@ -68,8 +68,8 @@ def prep_data(path, N_u=None, N_f=None, N_n=None, q=None, ub=None, lb=None, nois
 
     # Preparing the testing u_star
     u_star = Exact_u.flatten()[:,None]
-                
-    # Noiseless data TODO: add support for noisy data    
+
+    # Noiseless data TODO: add support for noisy data
     idx = np.random.choice(X_star.shape[0], N_u, replace=False)
     X_u_train = X_star[idx,:]
     u_train = u_star[idx,:]
@@ -80,37 +80,37 @@ def prep_data(path, N_u=None, N_f=None, N_n=None, q=None, ub=None, lb=None, nois
       x_0 = x[idx_x,:]
       u_0 = Exact_u[idx_x,idx_t_0][:,None]
       u_0 = u_0 + noise*np.std(u_0)*np.random.randn(u_0.shape[0], u_0.shape[1])
-          
+
       idx_x = np.random.choice(Exact_u.shape[0], N_1, replace=False)
       x_1 = x[idx_x,:]
       u_1 = Exact_u[idx_x,idx_t_1][:,None]
       u_1 = u_1 + noise*np.std(u_1)*np.random.randn(u_1.shape[0], u_1.shape[1])
-      
-      dt = np.asscalar(t[idx_t_1] - t[idx_t_0])        
+
+      dt = np.asscalar(t[idx_t_1] - t[idx_t_0])
       q = int(np.ceil(0.5*np.log(np.finfo(float).eps)/np.log(dt)))
 
       # Load IRK weights
       tmp = np.float32(np.loadtxt(os.path.join(utilsPath, "IRK_weights", "Butcher_IRK%d.txt" % (q)), ndmin = 2))
-      weights =  np.reshape(tmp[0:q**2+q], (q+1,q))     
+      weights =  np.reshape(tmp[0:q**2+q], (q+1,q))
       IRK_alpha = weights[0:-1,:]
-      IRK_beta = weights[-1:,:] 
+      IRK_beta = weights[-1:,:]
       return x_0, u_0, x_1, u_1, x, t, dt, q, Exact_u, IRK_alpha, IRK_beta
 
     if N_f == None:
       lb = X_star.min(axis=0)
-      ub = X_star.max(axis=0) 
+      ub = X_star.max(axis=0)
       return x, t, X, T, Exact_u, X_star, u_star, X_u_train, u_train, ub, lb
 
     # Domain bounds (lowerbounds upperbounds) [x, t], which are here ([-1.0, 0.0] and [1.0, 1.0])
     lb = X_star.min(axis=0)
-    ub = X_star.max(axis=0) 
+    ub = X_star.max(axis=0)
     # Getting the initial conditions (t=0)
     xx1 = np.hstack((X[0:1,:].T, T[0:1,:].T))
     uu1 = Exact_u[0:1,:].T
-    # Getting the lowest boundary conditions (x=-1) 
+    # Getting the lowest boundary conditions (x=-1)
     xx2 = np.hstack((X[:,0:1], T[:,0:1]))
     uu2 = Exact_u[:,0:1]
-    # Getting the highest boundary conditions (x=1) 
+    # Getting the highest boundary conditions (x=1)
     xx3 = np.hstack((X[:,-1:], T[:,-1:]))
     uu3 = Exact_u[:,-1:]
     # Stacking them in multidimensional tensors for training (X_u_train is for now the continuous boundaries)
@@ -140,13 +140,13 @@ def plot_inf_cont_results(X_star, u_pred, X_u_train, u_train, Exact_u, X, T, x, 
   fig, ax = newfig(1.0, 1.1)
   ax.axis('off')
 
-  ####### Row 0: u(t,x) ##################    
+  ####### Row 0: u(t,x) ##################
   gs0 = gridspec.GridSpec(1, 2)
   gs0.update(top=1-0.06, bottom=1-1/3, left=0.15, right=0.85, wspace=0)
   ax = plt.subplot(gs0[:, :])
 
-  h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow', 
-                extent=[t.min(), t.max(), x.min(), x.max()], 
+  h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow',
+                extent=[t.min(), t.max(), x.min(), x.max()],
                 origin='lower', aspect='auto')
   divider = make_axes_locatable(ax)
   cax = divider.append_axes("right", size="5%", pad=0.05)
@@ -157,29 +157,29 @@ def plot_inf_cont_results(X_star, u_pred, X_u_train, u_train, Exact_u, X, T, x, 
   line = np.linspace(x.min(), x.max(), 2)[:,None]
   ax.plot(t[25]*np.ones((2,1)), line, 'w-', linewidth = 1)
   ax.plot(t[50]*np.ones((2,1)), line, 'w-', linewidth = 1)
-  ax.plot(t[75]*np.ones((2,1)), line, 'w-', linewidth = 1)    
+  ax.plot(t[75]*np.ones((2,1)), line, 'w-', linewidth = 1)
 
   ax.set_xlabel('$t$')
   ax.set_ylabel('$x$')
   ax.legend(frameon=False, loc = 'best')
   ax.set_title('$u(t,x)$', fontsize = 10)
 
-  ####### Row 1: u(t,x) slices ##################    
+  ####### Row 1: u(t,x) slices ##################
   gs1 = gridspec.GridSpec(1, 3)
   gs1.update(top=1-1/3, bottom=0, left=0.1, right=0.9, wspace=0.5)
 
   ax = plt.subplot(gs1[0, 0])
-  ax.plot(x,Exact_u[25,:], 'b-', linewidth = 2, label = 'Exact')       
+  ax.plot(x,Exact_u[25,:], 'b-', linewidth = 2, label = 'Exact')
   ax.plot(x,U_pred[25,:], 'r--', linewidth = 2, label = 'Prediction')
   ax.set_xlabel('$x$')
-  ax.set_ylabel('$u(t,x)$')    
+  ax.set_ylabel('$u(t,x)$')
   ax.set_title('$t = 0.25$', fontsize = 10)
   ax.axis('square')
   ax.set_xlim([-1.1,1.1])
   ax.set_ylim([-1.1,1.1])
 
   ax = plt.subplot(gs1[0, 1])
-  ax.plot(x,Exact_u[50,:], 'b-', linewidth = 2, label = 'Exact')       
+  ax.plot(x,Exact_u[50,:], 'b-', linewidth = 2, label = 'Exact')
   ax.plot(x,U_pred[50,:], 'r--', linewidth = 2, label = 'Prediction')
   ax.set_xlabel('$x$')
   ax.set_ylabel('$u(t,x)$')
@@ -190,13 +190,13 @@ def plot_inf_cont_results(X_star, u_pred, X_u_train, u_train, Exact_u, X, T, x, 
   ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.35), ncol=5, frameon=False)
 
   ax = plt.subplot(gs1[0, 2])
-  ax.plot(x,Exact_u[75,:], 'b-', linewidth = 2, label = 'Exact')       
+  ax.plot(x,Exact_u[75,:], 'b-', linewidth = 2, label = 'Exact')
   ax.plot(x,U_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
   ax.set_xlabel('$x$')
   ax.set_ylabel('$u(t,x)$')
   ax.axis('square')
   ax.set_xlim([-1.1,1.1])
-  ax.set_ylim([-1.1,1.1])    
+  ax.set_ylim([-1.1,1.1])
   ax.set_title('$t = 0.75$', fontsize = 10)
 
   if save_path != None and save_hp != None:
@@ -208,51 +208,51 @@ def plot_inf_cont_results(X_star, u_pred, X_u_train, u_train, Exact_u, X, T, x, 
 def plot_inf_disc_results(x_star, idx_t_0, idx_t_1, x_0, u_0, ub, lb, u_1_pred, Exact_u, x, t, save_path=None, save_hp=None):
   fig, ax = newfig(1.0, 1.2)
   ax.axis('off')
-  
-  ####### Row 0: h(t,x) ##################    
+
+  ####### Row 0: h(t,x) ##################
   gs0 = gridspec.GridSpec(1, 2)
   gs0.update(top=1-0.06, bottom=1-1/2 + 0.1, left=0.15, right=0.85, wspace=0)
   ax = plt.subplot(gs0[:, :])
-  
-  h = ax.imshow(Exact_u.T, interpolation='nearest', cmap='rainbow', 
-                extent=[t.min(), t.max(), x_star.min(), x_star.max()], 
+
+  h = ax.imshow(Exact_u.T, interpolation='nearest', cmap='rainbow',
+                extent=[t.min(), t.max(), x_star.min(), x_star.max()],
                 origin='lower', aspect='auto')
   divider = make_axes_locatable(ax)
   cax = divider.append_axes("right", size="5%", pad=0.05)
   fig.colorbar(h, cax=cax)
-      
+
   line = np.linspace(x.min(), x.max(), 2)[:,None]
   ax.plot(t[idx_t_0]*np.ones((2,1)), line, 'w-', linewidth = 1)
   ax.plot(t[idx_t_1]*np.ones((2,1)), line, 'w-', linewidth = 1)
-  
+
   ax.set_xlabel('$t$')
   ax.set_ylabel('$x$')
   leg = ax.legend(frameon=False, loc = 'best')
   ax.set_title('$u(t,x)$', fontsize = 10)
-  
-  
-  ####### Row 1: h(t,x) slices ##################    
+
+
+  ####### Row 1: h(t,x) slices ##################
   gs1 = gridspec.GridSpec(1, 2)
   gs1.update(top=1-1/2-0.05, bottom=0.15, left=0.15, right=0.85, wspace=0.5)
-  
+
   ax = plt.subplot(gs1[0, 0])
-  ax.plot(x,Exact_u[idx_t_0,:], 'b-', linewidth = 2) 
-  ax.plot(x_0, u_0, 'rx', linewidth = 2, label = 'Data')      
+  ax.plot(x,Exact_u[idx_t_0,:], 'b-', linewidth = 2)
+  ax.plot(x_0, u_0, 'rx', linewidth = 2, label = 'Data')
   ax.set_xlabel('$x$')
-  ax.set_ylabel('$u(t,x)$')    
+  ax.set_ylabel('$u(t,x)$')
   ax.set_title('$t = %.2f$' % (t[idx_t_0]), fontsize = 10)
   ax.set_xlim([lb-0.1, ub+0.1])
   ax.legend(loc='upper center', bbox_to_anchor=(0.8, -0.3), ncol=2, frameon=False)
 
 
   ax = plt.subplot(gs1[0, 1])
-  ax.plot(x, Exact_u[idx_t_1,:], 'b-', linewidth = 2, label = 'Exact') 
-  ax.plot(x_star, u_1_pred, 'r--', linewidth = 2, label = 'Prediction')      
+  ax.plot(x, Exact_u[idx_t_1,:], 'b-', linewidth = 2, label = 'Exact')
+  ax.plot(x_star, u_1_pred, 'r--', linewidth = 2, label = 'Prediction')
   ax.set_xlabel('$x$')
-  ax.set_ylabel('$u(t,x)$')    
-  ax.set_title('$t = %.2f$' % (t[idx_t_1]), fontsize = 10)    
+  ax.set_ylabel('$u(t,x)$')
+  ax.set_title('$t = %.2f$' % (t[idx_t_1]), fontsize = 10)
   ax.set_xlim([lb-0.1, ub+0.1])
-  
+
   ax.legend(loc='upper center', bbox_to_anchor=(0.1, -0.3), ncol=2, frameon=False)
 
   if save_path != None and save_hp != None:
@@ -264,28 +264,28 @@ def plot_inf_disc_results(x_star, idx_t_0, idx_t_1, x_0, u_0, ub, lb, u_1_pred, 
 
 def plot_ide_disc_results(x_star, t_star, idx_t_0, idx_t_1, x_0, u_0, x_1, u_1,
   ub, lb, u_1_pred, Exact, lambda_1_value, lambda_1_value_noisy, lambda_2_value, lambda_2_value_noisy,
-  x, t, save_path=None, save_hp=None):  
+  x, t, save_path=None, save_hp=None):
   fig, ax = newfig(1.0, 1.5)
   ax.axis('off')
-  
+
   gs0 = gridspec.GridSpec(1, 2)
   gs0.update(top=1-0.06, bottom=1-1/3+0.05, left=0.15, right=0.85, wspace=0)
   ax = plt.subplot(gs0[:, :])
-      
+
   h = ax.imshow(Exact, interpolation='nearest', cmap='rainbow',
                 extent=[t_star.min(),t_star.max(), lb[0], ub[0]],
                 origin='lower', aspect='auto')
   divider = make_axes_locatable(ax)
   cax = divider.append_axes("right", size="5%", pad=0.05)
   fig.colorbar(h, cax=cax)
-  
+
   line = np.linspace(x_star.min(), x_star.max(), 2)[:,None]
   ax.plot(t_star[idx_t_0]*np.ones((2,1)), line, 'w-', linewidth = 1.0)
-  ax.plot(t_star[idx_t_1]*np.ones((2,1)), line, 'w-', linewidth = 1.0)    
+  ax.plot(t_star[idx_t_1]*np.ones((2,1)), line, 'w-', linewidth = 1.0)
   ax.set_xlabel('$t$')
   ax.set_ylabel('$x$')
   ax.set_title('$u(t,x)$', fontsize = 10)
-  
+
   gs1 = gridspec.GridSpec(1, 2)
   gs1.update(top=1-1/3-0.1, bottom=1-2/3, left=0.15, right=0.85, wspace=0.5)
 
@@ -295,7 +295,7 @@ def plot_ide_disc_results(x_star, t_star, idx_t_0, idx_t_1, x_0, u_0, x_1, u_1,
   ax.set_xlabel('$x$')
   ax.set_ylabel('$u(t,x)$')
   ax.set_title('$t = %.2f$\n%d trainng data' % (t_star[idx_t_0], u_0.shape[0]), fontsize = 10)
-  
+
   ax = plt.subplot(gs1[0, 1])
   ax.plot(x_star,Exact[:,idx_t_1][:,None], 'b', linewidth = 2, label = 'Exact')
   ax.plot(x_1, u_1, 'rx', linewidth = 2, label = 'Data')
@@ -303,10 +303,10 @@ def plot_ide_disc_results(x_star, t_star, idx_t_0, idx_t_1, x_0, u_0, x_1, u_1,
   ax.set_ylabel('$u(t,x)$')
   ax.set_title('$t = %.2f$\n%d trainng data' % (t_star[idx_t_1], u_1.shape[0]), fontsize = 10)
   ax.legend(loc='upper center', bbox_to_anchor=(-0.3, -0.3), ncol=2, frameon=False)
-  
+
   gs2 = gridspec.GridSpec(1, 2)
   gs2.update(top=1-2/3-0.05, bottom=0, left=0.15, right=0.85, wspace=0.0)
-  
+
   ax = plt.subplot(gs2[0, 0])
   ax.axis('off')
   nu = 0.01/np.pi
@@ -330,47 +330,47 @@ def plot_ide_cont_results(X_star, u_pred, X_u_train, u_train,
     ax.axis('off')
 
     U_pred = griddata(X_star, u_pred.flatten(), (X, T), method='cubic')
-    
-    ####### Row 0: u(t,x) ##################    
+
+    ####### Row 0: u(t,x) ##################
     gs0 = gridspec.GridSpec(1, 2)
     gs0.update(top=1-0.06, bottom=1-1.0/3.0+0.06, left=0.15, right=0.85, wspace=0)
     ax = plt.subplot(gs0[:, :])
-    
-    h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow', 
-                  extent=[t.min(), t.max(), x.min(), x.max()], 
+
+    h = ax.imshow(U_pred.T, interpolation='nearest', cmap='rainbow',
+                  extent=[t.min(), t.max(), x.min(), x.max()],
                   origin='lower', aspect='auto')
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
     fig.colorbar(h, cax=cax)
-    
+
     ax.plot(X_u_train[:,1], X_u_train[:,0], 'kx', label = 'Data (%d points)' % (u_train.shape[0]), markersize = 2, clip_on = False)
-    
+
     line = np.linspace(x.min(), x.max(), 2)[:,None]
     ax.plot(t[25]*np.ones((2,1)), line, 'w-', linewidth = 1)
     ax.plot(t[50]*np.ones((2,1)), line, 'w-', linewidth = 1)
     ax.plot(t[75]*np.ones((2,1)), line, 'w-', linewidth = 1)
-    
+
     ax.set_xlabel('$t$')
     ax.set_ylabel('$x$')
     ax.legend(loc='upper center', bbox_to_anchor=(1.0, -0.125), ncol=5, frameon=False)
     ax.set_title('$u(t,x)$', fontsize = 10)
-    
-    ####### Row 1: u(t,x) slices ##################    
+
+    ####### Row 1: u(t,x) slices ##################
     gs1 = gridspec.GridSpec(1, 3)
     gs1.update(top=1-1.0/3.0-0.1, bottom=1.0-2.0/3.0, left=0.1, right=0.9, wspace=0.5)
-    
+
     ax = plt.subplot(gs1[0, 0])
     ax.plot(x,Exact_u[25,:], 'b-', linewidth = 2, label = 'Exact')
     ax.plot(x,U_pred[25,:], 'r--', linewidth = 2, label = 'Prediction')
     ax.set_xlabel('$x$')
-    ax.set_ylabel('$u(t,x)$')    
+    ax.set_ylabel('$u(t,x)$')
     ax.set_title('$t = 0.25$', fontsize = 10)
     ax.axis('square')
     ax.set_xlim([-1.1,1.1])
     ax.set_ylim([-1.1,1.1])
-    
+
     ax = plt.subplot(gs1[0, 1])
-    ax.plot(x,Exact_u[50,:], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,Exact_u[50,:], 'b-', linewidth = 2, label = 'Exact')
     ax.plot(x,U_pred[50,:], 'r--', linewidth = 2, label = 'Prediction')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$u(t,x)$')
@@ -379,21 +379,21 @@ def plot_ide_cont_results(X_star, u_pred, X_u_train, u_train,
     ax.set_ylim([-1.1,1.1])
     ax.set_title('$t = 0.50$', fontsize = 10)
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.35), ncol=5, frameon=False)
-    
+
     ax = plt.subplot(gs1[0, 2])
-    ax.plot(x,Exact_u[75,:], 'b-', linewidth = 2, label = 'Exact')       
+    ax.plot(x,Exact_u[75,:], 'b-', linewidth = 2, label = 'Exact')
     ax.plot(x,U_pred[75,:], 'r--', linewidth = 2, label = 'Prediction')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$u(t,x)$')
     ax.axis('square')
     ax.set_xlim([-1.1,1.1])
-    ax.set_ylim([-1.1,1.1])    
+    ax.set_ylim([-1.1,1.1])
     ax.set_title('$t = 0.75$', fontsize = 10)
-    
-    ####### Row 3: Identified PDE ##################    
+
+    ####### Row 3: Identified PDE ##################
     gs2 = gridspec.GridSpec(1, 3)
     gs2.update(top=1.0-2.0/3.0, bottom=0, left=0.0, right=1.0, wspace=0.0)
-    
+
     ax = plt.subplot(gs2[:, :])
     ax.axis('off')
     s1 = r'$\begin{tabular}{ |c|c| }  \hline Correct PDE & $u_t + u u_x - 0.0031831 u_{xx} = 0$ \\  \hline Identified PDE (clean data) & '
